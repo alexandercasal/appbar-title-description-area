9ffa04c969ab4d3e3f7e005e1608d9ecf8faa89c
Alexander Casal <alexandericasal@gmail.com>
2018-04-03 00:29:50 -0600

    Pair support action bar with Toolbar in the activity's layout
    - Disable show title since we will be using our own TextView

--- app/src/main/java/com/alexandercasal/fadingedittextappbar/MainActivity.kt
+++ app/src/main/java/com/alexandercasal/fadingedittextappbar/MainActivity.kt
@@ -2,11 +2,15 @@ package com.alexandercasal.fadingedittextappbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.toolbar_main
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar_main)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
